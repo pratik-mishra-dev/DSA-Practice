610d1f25bbea7e7bec9a29bca84ea2617c827848
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-07-01 14:24:04 +0530

    Time: 8 ms (92.89%), Space: 47.2 MB (13.88%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,17 +1,26 @@
 class Solution {
+    public int find(int i,int j,int find,int[] arr){
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==find) return mid+1;
+            else if(arr[mid]>find) j=mid-1;
+            else i=mid+1;
+        }
+        return i;
+    }
     public int[] answerQueries(int[] nums, int[] queries) {
         int n=queries.length;
+        int m=nums.length;
         int[] arr=new int[n];
         Arrays.sort(nums);
-        int m=nums.length;
+        for(int i=1;i<m;i++)
+        nums[i]+=nums[i-1];
+
+        
         for(int i=0;i<n;i++){
-            int count=0;
-            int j=0;
-            while(j<m && count+nums[j]<=queries[i]){
-                count+=nums[j];
-                j++;
-            }
-            arr[i]=j;
+            int find=queries[i];
+            int ind=find(0,m-1,find,nums);
+            arr[i]=ind;
         }
         return arr;
     }
