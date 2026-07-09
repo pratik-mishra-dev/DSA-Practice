4d09633da65ca86c600d91812d1a4bfd88d0e6da
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-07-09 18:20:10 +0530

    Time: 0 ms (100%), Space: 42 MB (65.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int calculate(int m,int n,int[][] dp,int s,int e){
-        if(s==m-1|| e==n-1) return 1;
-        if(dp[s][e]!=0) return dp[s][e];
-        return dp[s][e]=calculate(m,n,dp,s+1,e)+calculate(m,n,dp,s,e+1);
-    }
+    // public int calculate(int m,int n,int[][] dp,int s,int e){
+    //     if(s==m-1|| e==n-1) return 1;
+    //     if(dp[s][e]!=0) return dp[s][e];
+    //     return dp[s][e]=calculate(m,n,dp,s+1,e)+calculate(m,n,dp,s,e+1);
+    // }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        int ans=calculate(m,n,dp,0,0);
-        return ans;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0) dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
