e6de4dc3a0d3e93cdaa0cd5f06bc0a865403df6f
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-07-13 00:33:19 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int fibo(int n,int[] dp){
-        if(n<=1) return n;
-        if(dp[n]!=0) return dp[n];
-        int ans=fibo(n-1,dp)+fibo(n-2,dp);
-        dp[n]=ans;
-        return ans;
-    }
     public int fib(int n) {
-        int[] dp=new int[n+1];
-        return fibo(n,dp);
+        if(n<=1) return n;
+        int f=0;
+        int s=1;
+        for(int i=1;i<n;i++){
+            int temp=s;
+            s=s+f;
+            f=temp;
+        }
+        return s;
     }
 }
