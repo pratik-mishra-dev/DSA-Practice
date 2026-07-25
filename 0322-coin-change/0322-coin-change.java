b676d34e777b1a3a6fa0fda5daeb402d3b87540c
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-07-25 13:41:21 +0530

    Time: 50 ms (5.08%), Space: 47.9 MB (5.01%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long count(int i,int[] coins,int amount, long[][] dp){
+        if(i==coins.length){
+            if(amount==0) return 0;
+            else return Integer.MAX_VALUE;
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        long skip=count(i+1,coins,amount,dp);
+        if(amount-coins[i]<0) return dp[i][amount]=skip;
+        long pick=1+count(i,coins,amount-coins[i],dp);
+
+        return dp[i][amount]=Math.min(skip,pick);
+    }
+    public int coinChange(int[] coins, int amount) {
+        long[][] dp=new long[coins.length][amount+1];
+        for (long[] row : dp)
+        Arrays.fill(row,-1);
+
+       int ans=(int)count(0,coins,amount,dp);
+       if(ans==Integer.MAX_VALUE) return -1;
+       return ans;
+    }
+}
