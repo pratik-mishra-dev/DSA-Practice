5f3953a6e3d5ad209a8b1c8dbbb56ef6e6d6e594
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-04 00:05:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (51.18%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -2,21 +2,13 @@ class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int n=tickets.length;
         int t=0;
-        while(tickets[k]!=1){
-            for(int i=0;i<n;i++){
-                if(tickets[i]==0) continue;
-                else{
-                    tickets[i]--;
-                    t++;
-                }
-            }
-        }
-        for(int i=0;i<=k;i++){
-                if(tickets[i]==0) continue;
-                else{
-                    tickets[i]--;
-                    t++;
-                }
+        for(int i=0;i<n;i++){
+              if(i<=k){
+                t+=Math.min(tickets[i],tickets[k]);
+              }
+              else{
+                t+=Math.min(tickets[i],tickets[k]-1);
+              }
             }
             return t;
     }
