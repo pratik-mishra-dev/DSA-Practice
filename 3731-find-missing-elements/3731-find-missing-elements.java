b079e5f58b2b08b829fdcccbdfa7b78c804455ac
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-04 11:54:52 +0530

    Time: 7 ms (22.5%), Space: 46.6 MB (80.06%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,21 +1,36 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        List<Integer> l=new ArrayList<>();
-       int start=nums[0];
-       int end=nums[n-1];
-       int i=1;
-       while(start!=end){
-        if(i<n && nums[i]==start+1){
-             i++;
-             start++;
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        // Add nums to pq1 and find min/max
+        for (int num : nums) {
+            pq1.offer(num);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        else{
-            l.add(start+1);
-            start++;
+
+        // Add all numbers in the range to pq2
+        for (int i = min; i <= max; i++) {
+            pq2.offer(i);
         }
-       }
-        return l;
+
+        List<Integer> ans = new ArrayList<>();
+
+        while (!pq2.isEmpty()) {
+            if (!pq1.isEmpty() && pq1.peek().equals(pq2.peek())) {
+                pq1.poll();
+                pq2.poll();
+            } else {
+                ans.add(pq2.poll());
+            }
+        }
+
+        return ans;
     }
 }
