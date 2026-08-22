a2400937190aaecac15e46a1b83a795a3b5b56d5
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-22 14:41:50 +0530

    Time: 0 ms (100%), Space: 42.4 MB (66.14%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
         int x=0;
-        String s=Integer.toBinaryString(n);
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1') x++;
+        while(n!=0){
+            n=n&n-1;
+            x++;
         }
         return x;
-    }
+    } 
 }
