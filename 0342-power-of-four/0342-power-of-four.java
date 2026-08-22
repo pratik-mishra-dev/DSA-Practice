9d0413060cdbf7a62fe84c699b3bce6b8515cf9a
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-22 16:17:09 +0530

    Time: 1 ms (89.75%), Space: 42.4 MB (86.11%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -2,6 +2,11 @@ class Solution {
     public boolean isPowerOfFour(int n) {
         if(n==1) return true;
         if(n<=0 || n%4!=0) return false;
-         return isPowerOfFour(n/4);
+        for(int i=0;i<=31;i+=2){
+            int y=1<<i;
+            if(y==n) return true;
+        }
+        return false;
+        
     }
 }
