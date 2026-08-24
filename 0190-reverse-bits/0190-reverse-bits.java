12a5d6e0ad6dbcedf0c536950a3dbd6d1b9e73fd
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-24 23:33:16 +0530

    Time: 1 ms (60.96%), Space: 42.6 MB (34.29%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,8 +1,16 @@
 class Solution {
     public int reverseBits(int n) {
-        String binary = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
-        String reversed = new StringBuilder(binary).reverse().toString();
-        return Integer.parseUnsignedInt(reversed, 2);
-    
+        int i=0;
+        int j=31;
+        while(i<j){
+            int imask=1<<i,  jmask=1<<j;
+            boolean ion= (n&imask)!=0, jon=(n&jmask)!=0;
+            if(ion != jon){// change th bit of both
+                n^=imask; n^=jmask;
+            }
+            i++;
+            j--;
+        }
+        return n;
     }
 }
