7ed9559ac587749b6a314dd61151aea1a51083ce
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-08-25 23:06:20 +0530

    Time: 13 ms (65.74%), Space: 43.3 MB (9.41%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -3,8 +3,8 @@ class Solution {
         int n = arr.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
                     if (Math.abs(arr[i] - arr[j]) <= a &&
                             Math.abs(arr[j] - arr[k]) <= b &&
                             Math.abs(arr[i] - arr[k]) <= c) {
