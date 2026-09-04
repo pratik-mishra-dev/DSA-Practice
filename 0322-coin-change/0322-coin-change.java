cb350ad0b17421b62678f241923b4d7b0f4a5b90
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-04 22:11:45 +0530

    Time: 38 ms (9.41%), Space: 48.3 MB (5.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,25 @@
 class Solution {
-    public long count(int i,int[] coins,int amount, long[][] dp){
-        if(i==coins.length){
+    public long count(int ind,int[] coins, int amount, long[][] dp){
+        if(ind==coins.length){
             if(amount==0) return 0;
             else return Integer.MAX_VALUE;
         }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        long skip=count(i+1,coins,amount,dp);
-        if(amount-coins[i]<0) return dp[i][amount]=skip;
-        long pick=1+count(i,coins,amount-coins[i],dp);
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
 
-        return dp[i][amount]=Math.min(skip,pick);
+        long skip=count(ind+1,coins,amount,dp);
+        if(amount-coins[ind]<0) return dp[ind][amount]=skip;
+        long pick=1+count(ind,coins,amount-coins[ind],dp); 
+        return dp[ind][amount]=Math.min(pick,skip);
     }
     public int coinChange(int[] coins, int amount) {
         long[][] dp=new long[coins.length][amount+1];
-        for (long[] row : dp)
-        Arrays.fill(row,-1);
 
-       int ans=(int)count(0,coins,amount,dp);
-       if(ans==Integer.MAX_VALUE) return -1;
-       return ans;
+        for(long[] ele:dp)
+        Arrays.fill(ele,-1);
+
+        int ans=(int)count(0,coins,amount,dp);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
+
     }
 }
