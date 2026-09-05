435fd081f85ef92d725b355665b2ba4ef874667c
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-05 23:22:48 +0530

    Time: 1 ms (99.92%), Space: 46.5 MB (40.09%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isvalid(char[][] board,int row,int col,char num){
+        // check col
+        for(int i=0;i<9;i++){
+            if(board[i][col]==num) return false;
+        }
+        // check row
+        for(int i=0;i<9;i++){
+            if(board[row][i]==num) return false;
+        }
+
+        //imp math and obserbation
+        int srow=row/3*3;
+        int scol=col/3*3;
+        for(int i=srow;i<srow+3;i++){
+            for(int j=scol;j<scol+3;j++){
+                if(board[i][j]==num) return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.')
+                    continue;
+                board[i][j] = '.';
+                if (isvalid(board, i, j,num) == false)
+                    return false;
+                board[i][j] = num;
+            }
+        }
+        return true;
+    }
+}
