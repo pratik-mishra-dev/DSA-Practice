cf74c9a0c39b11c0fd34fbee0273d2250e562d43
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-06 19:03:39 +0530

    Time: 731 ms (5.05%), Space: 42.9 MB (58.71%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,59 @@
+class Solution {
+    char[][] grid;
+    public boolean isvalid(char[][] board,int row,int col,char num){
+        // check col
+        for(int i=0;i<9;i++){
+            if(board[i][col]==num) return false;
+        }
+        // check row
+        for(int i=0;i<9;i++){
+            if(board[row][i]==num) return false;
+        }
+
+        //imp math and obserbation
+        int srow=row/3*3;//this 2 line very imp
+        int scol=col/3*3;
+        for(int i=srow;i<srow+3;i++){
+            for(int j=scol;j<scol+3;j++){
+                if(board[i][j]==num) return false;
+            }
+        }
+        return true;
+    }
+    public void solve(char[][] board,int i,int j){
+        if(i==9){
+            for(int k=0;k<9;k++){
+                for(int l=0;l<9;l++){
+                    grid[k][l]=board[k][l];
+                }
+            }
+
+            return;
+        }
+        if(board[i][j]!='.'){
+            if(j!=8) solve(board,i,j+1);
+            else solve(board,i+1,0);
+        }
+        else{
+            for(char ch='1';ch<='9';ch++){
+                if(isvalid(board,i,j,ch)){
+                    board[i][j]=ch;
+                    if(j!=8) solve(board,i,j+1);
+                    else solve(board,i+1,0);
+                     board[i][j]='.';
+                }
+            }
+
+        }
+    }
+    public void solveSudoku(char[][] board) {
+         grid=new char[9][9];
+        solve(board,0,0);
+       
+         for(int i=0;i<9;i++){
+                for(int j=0;j<9;j++){
+                    board[i][j]=grid[i][j];
+                }
+            }
+    }
+}
