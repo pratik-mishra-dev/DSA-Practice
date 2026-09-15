cbab1d2c16997460d3f017ca999e8b8a0ec465d7
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-15 10:33:41 +0530

    Time: 1 ms (85.43%), Space: 47 MB (94.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,31 @@
 class Solution {
-    public void bfs(int i,int[][] adj,boolean[] visited){
-        int n=adj.length;
-       visited[i]=true;
+    public void bfs(int i,int[][] nums,boolean[] visited){
+        int n=nums.length;
         Queue<Integer> q=new ArrayDeque<>();
+        visited[i]=true;
         q.add(i);
-            while(q.size()>0){
-                int front=q.remove();
-                for(int j=0;j<n;j++){
-                    if(adj[front][j]==1 && visited[j]==false){
-                        q.add(j);
-                        visited[j]=true;
-                    }
+
+        while(q.size()>0){
+            int front=q.remove();
+            for(int j=0;j<n;j++){
+                if(nums[front][j]==1 && visited[j]==false){
+                    q.add(j);
+                    visited[j]=true;
                 }
             }
-       }
-    public int findCircleNum(int[][] adj){
-        int n=adj.length;  
-        int count=0;
+        }
+    }
+    public int findCircleNum(int[][] nums) {
+        int n=nums.length;
         boolean[] visited=new boolean[n];
+        int count=0;
 
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                bfs(i,adj,visited);
+            if(visited[i]==false){
                 count++;
+                bfs(i,nums,visited);
             }
         }
-
         return count;
-        
     }
 }
