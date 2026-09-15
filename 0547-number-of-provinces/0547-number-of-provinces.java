34ca7768c9343c39bbe42a8bc7c0f12bdbf83e2e
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-15 10:37:20 +0530

    Time: 1 ms (85.43%), Space: 47.1 MB (87.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,19 +1,13 @@
 class Solution {
-    public void bfs(int i,int[][] nums,boolean[] visited){
+    public void dfs(int i,int[][] nums,boolean[] visited){
         int n=nums.length;
-        Queue<Integer> q=new ArrayDeque<>();
         visited[i]=true;
-        q.add(i);
-
-        while(q.size()>0){
-            int front=q.remove();
-            for(int j=0;j<n;j++){
-                if(nums[front][j]==1 && visited[j]==false){
-                    q.add(j);
+        for(int j=0;j<n;j++){
+                if(nums[i][j]==1 && visited[j]==false){
                     visited[j]=true;
+                     dfs(j,nums,visited);
                 }
             }
-        }
     }
     public int findCircleNum(int[][] nums) {
         int n=nums.length;
@@ -23,7 +17,8 @@ class Solution {
         for(int i=0;i<n;i++){
             if(visited[i]==false){
                 count++;
-                bfs(i,nums,visited);
+                visited[i]=true;
+                dfs(i,nums,visited);
             }
         }
         return count;
