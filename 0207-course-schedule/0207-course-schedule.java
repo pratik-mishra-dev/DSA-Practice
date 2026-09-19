2e6c892237b8ada68ca913604785a8839af0ae44
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-19 09:40:31 +0530

    Time: 4 ms (91.52%), Space: 46.9 MB (77.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,23 @@
 class Solution {
+    boolean ans=true;
+    public void dfs(int i, boolean[] visit, boolean[] path, List<List<Integer>> l){
+       visit[i]=true;
+        path[i]=true;
+        for(int ele:l.get(i)){
+            if(path[ele]){
+                ans=false;
+                return;
+            }
+             if (!visit[ele]) {
+                dfs(ele, visit, path, l);
+                if (!ans) return;
+            }
+              
+        }
+        path[i]=false;
+
+    }
     public boolean canFinish(int n, int[][] prerequisites) {
-         int[] arr=new int[n];
         int m=prerequisites.length;
 
         List<List<Integer>> l=new ArrayList<>();
@@ -14,34 +31,16 @@ class Solution {
             int b=prerequisites[i][1];
 
             l.get(b).add(a);
-
-            arr[a]++;
         }
 
-        Queue<Integer> q=new ArrayDeque<>();
-        int[] ans=new int[n];
-        int k=0;
+        boolean[] visit=new boolean[n];
+        boolean[] path=new boolean[n];
 
         for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                ans[k++]=i;
-                q.add(i);
-            }
+            if(visit[i]==false) dfs(i,visit,path,l);
         }
 
-        while(q.size()>0){
-            int x=q.remove();
+        return ans;
 
-            for(int ele:l.get(x)){
-                 arr[ele]--;
-                if(arr[ele]==0){
-                ans[k++]=ele;
-                q.add(ele);
-                 }
-                }
-
-        }
-        if(k!=n) return false;
-        return true;
     }
 }
