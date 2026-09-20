e99f9d438a1e1f74f2682a03fcdbf43fa2bd4f52
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-20 09:17:33 +0530

    Time: 2 ms (56.41%), Space: 46.4 MB (68.12%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+
+        int[] freq1=new int[26];
+        int[] freq2=new int[26];
+
+        for(int i=0;i<n;i++){
+            char c=s1.charAt(i);
+            freq1[c-'a']++;
+        }
+
+        for(int i=0;i<m;i++){
+            char c=s2.charAt(i);
+            freq2[c-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq1[i]>freq2[i]) return false;
+        }
+
+        return true;
+    }
+}
