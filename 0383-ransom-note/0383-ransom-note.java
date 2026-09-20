36e9aa5cc656d54aa49abf8ee34469b513575198
Pratik Mishra <pratikmishra.outr.in@gmail.com>
2026-09-20 09:19:58 +0530

    Time: 1 ms (99.95%), Space: 46.6 MB (48.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -6,20 +6,16 @@ class Solution {
         int[] freq1=new int[26];
         int[] freq2=new int[26];
 
-        for(int i=0;i<n;i++){
-            char c=s1.charAt(i);
-            freq1[c-'a']++;
-        }
-
         for(int i=0;i<m;i++){
             char c=s2.charAt(i);
             freq2[c-'a']++;
         }
-
-        for(int i=0;i<26;i++){
-            if(freq1[i]>freq2[i]) return false;
+        
+        for(int i=0;i<n;i++){
+            char c=s1.charAt(i);
+            freq1[c-'a']++;
+             if(freq1[c-'a']>freq2[c-'a']) return false;
         }
-
         return true;
     }
 }
